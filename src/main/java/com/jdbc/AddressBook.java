bd9ac5a3b97f6bf3804331a6f5ed300c55496716
drisyaathul <drisyak809@gmail.com>
2023-06-03 17:05:03 +0800

    Address Book - CRUD Operation

--- src/main/java/com/jdbc/AddressBook.java
+++ src/main/java/com/jdbc/AddressBook.java
@@ -12,12 +12,12 @@ public class AddressBook {
             connection.setAutoCommit(false);
             try (Statement statement = connection.createStatement()) {
                 String createTableQuery = "create table addressbook (name varchar(20), address varchar(60), city varchar(20), state varchar(20), zip int, phoneNumber int)";
-                //statement.executeUpdate(createTableQuery);
+                statement.executeUpdate(createTableQuery);
                 System.out.println("Table Created Successfully");
             }
             try (Statement statement = connection.createStatement()) {
                 String insertQuery = "insert into addressbook (name,address,city,state,zip,phoneNumber)values('Drisya','sreyas','kualaLumpur','malaysia',25874,999999999),('Varsha','gana','calicut','kerala',12365,888888888),('Kavya','gaya','bangalore','karnataka',21456,777777777)";
-                //statement.executeUpdate(insertQuery);
+                statement.executeUpdate(insertQuery);
                 System.out.println("Inserted Successfully");
             }
             try (Statement statement = connection.createStatement()) {
